8fafb17ccf36ae12f3817adb9d02d2c16c972cfc
Wansan126214 <1828415300@qq.com>
2026-05-12 17:52:27 +0800

    推送测试

--- Tomato_Brother/Assets/Script/test.cs
+++ Tomato_Brother/Assets/Script/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello world");
+        Debug.Log("hello world111");
     }
 
     // Update is called once per frame
